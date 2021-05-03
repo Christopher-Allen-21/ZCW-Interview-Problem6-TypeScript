2850826a643038b632d70dd8852487b10c46f70f
Chris Allen <callen7908@yahoo.com>
2021-05-03 00:04:30 -0400

    It works

--- time-app/src/App.js
+++ time-app/src/App.js
@@ -7,8 +7,8 @@ import MilitaryTimeRow from './components/MilitaryTimeRow';
 
 
 function App() {
-  const [hourOptions, setHourOptions] = useState(["01","02","03","04","05","06","07","08","09","10","11","12"]);
-  const [minuteOptions, setMinuteOptions] = useState(["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59"]);
+  const [hourOptions, setHourOptions] = useState(["00","01","02","03","04","05","06","07","08","09","10","11","12"]);
+  const [minuteOptions, setMinuteOptions] = useState(["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59"]);
   const [amPmOptions, setAmPmOptions] = useState(["AM","PM"]);
 
   const [hour, setHour] = useState();
@@ -32,9 +32,6 @@ function App() {
         <li><AmPmRow id="am-pm" amPmOptions={amPmOptions} onChangeAmPm={e => setAmPm(e.target.value)}/></li>
       </ol>
 
-      <button onClick="handleButtonClick">Convert the Time!</button>
-
-      <p>Hour: {hour} Minute: {minute} AM/PM: {amPm}</p> 
       <MilitaryTimeRow selectedHour={hour} selectedMinute={minute} selectedAmPm={amPm}/>
         
     </div>
--- time-app/src/components/MilitaryTimeRow.js
+++ time-app/src/components/MilitaryTimeRow.js
@@ -8,9 +8,268 @@ export default function MilitaryTimeRow(props){
         selectedAmPm
     } = props
 
+    let convertedHour = "Zero";
+    let convertedMinute = "Zero";   
+
+    if(selectedAmPm === "AM"){
+        if(selectedHour === "01"){
+            convertedHour = "Zero One";
+        }
+        else if(selectedHour === "02"){
+            convertedHour = "Zero Two";
+        }
+        else if(selectedHour === "03"){
+            convertedHour = "Zero Three";
+        }
+        else if(selectedHour === "04"){
+            convertedHour = "Zero Four";
+        }
+        else if(selectedHour === "05"){
+            convertedHour = "Zero Five";
+        }
+        else if(selectedHour === "06"){
+            convertedHour = "Zero Six";
+        }
+        else if(selectedHour === "07"){
+            convertedHour = "Zero Seven";
+        }
+        else if(selectedHour === "08"){
+            convertedHour = "Zero Eight";
+        }
+        else if(selectedHour === "09"){
+            convertedHour = "Zero Nine";
+        }
+        else if(selectedHour === "10"){
+            convertedHour = "Ten";
+        }
+        else if(selectedHour === "11"){
+            convertedHour = "Eleven";
+        }
+        else if(selectedHour === "12"){
+            convertedHour = "Zero"
+        }
+    }
+    else if(selectedAmPm === "PM"){
+        if(selectedHour === "01"){
+            convertedHour = "Thirteen";
+        }
+        else if(selectedHour === "02"){
+            convertedHour = "Fourteen";
+        }
+        else if(selectedHour === "03"){
+            convertedHour = "Fifteen";
+        }
+        else if(selectedHour == "04"){
+            convertedHour = "Sixteen";
+        }
+        else if(selectedHour === "05"){
+            convertedHour = "Seventeen";
+        }
+        else if(selectedHour === "06"){
+            convertedHour = "Eighteen";
+        }
+        else if(selectedHour === "07"){
+            convertedHour = "Nineteen";
+        }
+        else if(selectedHour === "08"){
+            convertedHour = "Twenty";
+        }
+        else if(selectedHour === "09"){
+            convertedHour = "Twenty-One";
+        }
+        else if(selectedHour === "10"){
+            convertedHour = "Twenty-Two";
+        }
+        else if(selectedHour === "11"){
+            convertedHour = "Twenty-Three";
+        }
+        else if(selectedHour === "12"){
+            convertedHour = "Twelve";
+        }
+    }
+
+    if(selectedMinute === "01"){
+        convertedMinute = "One";
+    }
+    else if(selectedMinute === "02"){
+        convertedMinute = "Two"
+    }
+    else if(selectedMinute === "03"){
+        convertedMinute = "Three"
+    }
+    else if(selectedMinute === "04"){
+        convertedMinute = "Four"
+    }
+    else if(selectedMinute === "05"){
+        convertedMinute = "Five"
+    }
+    else if(selectedMinute === "06"){
+        convertedMinute = "Six"
+    }
+    else if(selectedMinute === "07"){
+        convertedMinute = "Seven"
+    }
+    else if(selectedMinute === "08"){
+        convertedMinute = "Eight"
+    }
+    else if(selectedMinute === "09"){
+        convertedMinute = "Nine"
+    }
+    else if(selectedMinute === "10"){
+        convertedMinute = "Ten"
+    }
+    else if(selectedMinute === "11"){
+        convertedMinute = "Eleven";
+    }
+    else if(selectedMinute === "12"){
+        convertedMinute = "Twelve"
+    }
+    else if(selectedMinute === "13"){
+        convertedMinute = "Thirteen"
+    }
+    else if(selectedMinute === "14"){
+        convertedMinute = "Fourteen"
+    }
+    else if(selectedMinute === "15"){
+        convertedMinute = "Fifteen"
+    }
+    else if(selectedMinute === "16"){
+        convertedMinute = "Sixteen"
+    }
+    else if(selectedMinute === "17"){
+        convertedMinute = "Seventeen"
+    }
+    else if(selectedMinute === "18"){
+        convertedMinute = "Eighteen"
+    }
+    else if(selectedMinute === "19"){
+        convertedMinute = "Nineteen"
+    }
+    else if(selectedMinute === "20"){
+        convertedMinute = "Twenty"
+    }
+    else if(selectedMinute === "21"){
+        convertedMinute = "Twenty-One";
+    }
+    else if(selectedMinute === "22"){
+        convertedMinute = "Twenty-Two"
+    }
+    else if(selectedMinute === "23"){
+        convertedMinute = "Twenty-Three"
+    }
+    else if(selectedMinute === "24"){
+        convertedMinute = "Twenty-Four"
+    }
+    else if(selectedMinute === "25"){
+        convertedMinute = "Twenty-Five"
+    }
+    else if(selectedMinute === "26"){
+        convertedMinute = "Twenty-Six"
+    }
+    else if(selectedMinute === "27"){
+        convertedMinute = "Twenty-Seven"
+    }
+    else if(selectedMinute === "28"){
+        convertedMinute = "Twenty-Eight"
+    }
+    else if(selectedMinute === "29"){
+        convertedMinute = "Twenty-Nine"
+    }
+    else if(selectedMinute === "30"){
+        convertedMinute = "Thirty"
+    }
+    else if(selectedMinute === "31"){
+        convertedMinute = "Thirty-One";
+    }
+    else if(selectedMinute === "32"){
+        convertedMinute = "Thirty-Two"
+    }
+    else if(selectedMinute === "33"){
+        convertedMinute = "Thirty-Three"
+    }
+    else if(selectedMinute === "34"){
+        convertedMinute = "Thirty-Four"
+    }
+    else if(selectedMinute === "35"){
+        convertedMinute = "Thirty-Five"
+    }
+    else if(selectedMinute === "36"){
+        convertedMinute = "Thirty-Six"
+    }
+    else if(selectedMinute === "37"){
+        convertedMinute = "Thirty-Seven"
+    }
+    else if(selectedMinute === "38"){
+        convertedMinute = "Thirty-Eight"
+    }
+    else if(selectedMinute === "39"){
+        convertedMinute = "Thirty-Nine"
+    }
+    else if(selectedMinute === "40"){
+        convertedMinute = "Forty"
+    }
+    else if(selectedMinute === "41"){
+        convertedMinute = "Forty-One";
+    }
+    else if(selectedMinute === "42"){
+        convertedMinute = "Forty-Two"
+    }
+    else if(selectedMinute === "43"){
+        convertedMinute = "Forty-Three"
+    }
+    else if(selectedMinute === "44"){
+        convertedMinute = "Forty-Four"
+    }
+    else if(selectedMinute === "45"){
+        convertedMinute = "Forty-Five"
+    }
+    else if(selectedMinute === "46"){
+        convertedMinute = "Forty-Six"
+    }
+    else if(selectedMinute === "47"){
+        convertedMinute = "Forty-Seven"
+    }
+    else if(selectedMinute === "48"){
+        convertedMinute = "Forty-Eight"
+    }
+    else if(selectedMinute === "49"){
+        convertedMinute = "Forty-Nine"
+    }
+    else if(selectedMinute === "50"){
+        convertedMinute = "Fifty"
+    }
+    else if(selectedMinute === "51"){
+        convertedMinute = "Fifty-One";
+    }
+    else if(selectedMinute === "52"){
+        convertedMinute = "Fifty-Two"
+    }
+    else if(selectedMinute === "53"){
+        convertedMinute = "Fifty-Three"
+    }
+    else if(selectedMinute === "54"){
+        convertedMinute = "Fifty-Four"
+    }
+    else if(selectedMinute === "55"){
+        convertedMinute = "Fifty-Five"
+    }
+    else if(selectedMinute === "56"){
+        convertedMinute = "Fifty-Six"
+    }
+    else if(selectedMinute === "57"){
+        convertedMinute = "Fifty-Seven"
+    }
+    else if(selectedMinute === "58"){
+        convertedMinute = "Fifty-Eight"
+    }
+    else if(selectedMinute === "59"){
+        convertedMinute = "Fifty-Nine"
+    }
+  
+
     return (
         <div>
-            <p>The current Military Time is {selectedHour} Hundred {selectedMinute} Hours {selectedAmPm}</p>
+            <p>The current Military Time is: <strong>{convertedHour} Hundred {convertedMinute} Hours</strong></p>
             <br />
         </div>
     )
